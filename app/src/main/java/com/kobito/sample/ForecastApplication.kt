5fb0ea5f601d09c34745bcdec104997068afb3bb
kotibo <11027517+kobito@users.noreply.github.com>
2019-01-03 06:10:14 +0900

    apply CurrentWeatherViewModel to CurrenWeatherFragment
    test implementation

--- app/src/main/java/com/kobito/sample/ForecastApplication.kt
+++ app/src/main/java/com/kobito/sample/ForecastApplication.kt
@@ -10,11 +10,13 @@ import com.kobito.sample.data.network.WeatherNetworkDataSource
 import com.kobito.sample.data.network.WeatherNetworkDataSourceImpl
 import com.kobito.sample.data.repository.ForecastRepository
 import com.kobito.sample.data.repository.ForecastRepositoryImpl
+import com.kobito.sample.ui.weather.current.CurrentWeatherViewModelFactory
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.androidXModule
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
 import org.kodein.di.generic.singleton
 
 class ForecastApplication : Application(), KodeinAware {
@@ -27,6 +29,7 @@ class ForecastApplication : Application(), KodeinAware {
         bind() from singleton { ApixuWeatherApiService(instance()) }
         bind<WeatherNetworkDataSource>() with singleton { WeatherNetworkDataSourceImpl(instance()) }
         bind<ForecastRepository>() with singleton { ForecastRepositoryImpl(instance(), instance()) }
+        bind() from provider { CurrentWeatherViewModelFactory(instance()) }
     }
 
     override fun onCreate() {
--- app/src/main/java/com/kobito/sample/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/kobito/sample/ui/weather/current/CurrentWeatherFragment.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.kobito.sample.R
+import com.kobito.sample.ui.base.ScopedFragment
+import kotlinx.android.synthetic.main.current_weather_fragment.test
+import kotlinx.coroutines.launch
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.instance
 
-class CurrentWeatherFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = CurrentWeatherFragment()
-    }
+class CurrentWeatherFragment : ScopedFragment(), KodeinAware {
 
     private lateinit var viewModel: CurrentWeatherViewModel
+    override val kodein by closestKodein()
+    private val viewModelFactory: CurrentWeatherViewModelFactory by instance()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,7 +30,17 @@ class CurrentWeatherFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(CurrentWeatherViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(CurrentWeatherViewModel::class.java)
+
+        bindUI()
+    }
+
+    private fun bindUI() = launch {
+        val currentWeather = viewModel.weather.await()
+        currentWeather.observe(this@CurrentWeatherFragment, Observer {
+            if (it == null) return@Observer
+
+            test.text = it.toString()
+        })
     }
 }
