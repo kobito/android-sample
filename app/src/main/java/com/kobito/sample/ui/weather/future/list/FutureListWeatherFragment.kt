0192ba440d8407ce7d08ff3346be6d9b787784a4
kobitoC <11027517+kobito@users.noreply.github.com>
2019-01-22 10:12:20 +0900

    first implementation of FutureListWeatherFragment

--- app/src/main/java/com/kobito/sample/ui/weather/future/list/FutureListWeatherFragment.kt
+++ app/src/main/java/com/kobito/sample/ui/weather/future/list/FutureListWeatherFragment.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.kobito.sample.R
+import com.kobito.sample.ui.base.ScopedFragment
+import kotlinx.android.synthetic.main.future_list_weather_fragment.group_loading
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.instance
 
-class FutureListWeatherFragment : Fragment() {
+class FutureListWeatherFragment : ScopedFragment(), KodeinAware {
 
-    companion object {
-        fun newInstance() = FutureListWeatherFragment()
-    }
+    override val kodein by closestKodein()
+    private val viewModelFactory: FutureListWeatherViewModelFactory by instance()
 
     private lateinit var viewModel: FutureListWeatherViewModel
 
@@ -26,7 +33,34 @@ class FutureListWeatherFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(FutureListWeatherViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+            .get(FutureListWeatherViewModel::class.java)
+        bindUI()
+    }
+
+    private fun bindUI() = launch(Dispatchers.Main) {
+        val futureWeatherEntries = viewModel.weatherEntries.await()
+        val weatherLocation = viewModel.weatherLocation.await()
+
+        weatherLocation.observe(this@FutureListWeatherFragment, Observer { location ->
+            if (location == null) return@Observer
+
+            updateLocation(location.name)
+        })
+
+        futureWeatherEntries.observe(this@FutureListWeatherFragment, Observer { weatherEntries ->
+            if (weatherEntries == null) return@Observer
+
+            group_loading.visibility = View.GONE
+            updateDateToNextWeek()
+        })
+    }
+
+    private fun updateLocation(location: String) {
+        (activity as? AppCompatActivity)?.supportActionBar?.title = location
+    }
+
+    private fun updateDateToNextWeek() {
+        (activity as? AppCompatActivity)?.supportActionBar?.subtitle = "Next Week"
     }
 }
